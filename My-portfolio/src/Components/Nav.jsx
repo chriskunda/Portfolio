162c310aee096f47d7b9b6133a6f96ab3bafea7c
chriskunda <christianira2003@gmail.com>
2025-10-08 23:17:47 +0200

    navbar

---
+++ My-portfolio/src/Components/Nav.jsx
@@ -0,0 +1,22 @@
+import "./Nav.css"
+function Nav() {
+  return( 
+  <div className="nav">
+  <nav>
+    <div className="chris">
+    <p>Chris</p>
+    </div>    
+    <div className="links">
+    <a href="">Home</a>
+    <a href="">Projects</a>
+    <a href="">Skills</a>
+    </div>
+    <div className="butt">
+    <button>Contact</button>
+    </div>
+  </nav>
+  </div>
+  )
+}
+
+export default Nav;
