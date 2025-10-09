8ad00c06fc37212e6c22a0cce24aa4a0ca0aa4ca
chriskunda <christianira2003@gmail.com>
2025-10-09 23:34:27 +0200

    mid body1

--- My-portfolio/src/App.jsx
+++ My-portfolio/src/App.jsx
@@ -1,10 +1,11 @@
 import Nav from  "./Components/Nav";
+import Body1 from "./Components/Body1";
 
 function App() {
   return (
     <div>
       <Nav />
-      
+      <Body1 />
     </div>
   );
 }
---
+++ My-portfolio/src/Components/Body1.jsx
@@ -0,0 +1,20 @@
+import "./Body1.css"
+
+
+function Body1 () {
+    return (
+        <div className="body1">
+            <div className="names">
+                <h2>Hey, I'm</h2>
+                <h1>CHRISTIAN</h1>
+                <h1>IRADUKUNDA</h1>
+            </div>
+            <div className="foto">
+                
+            </div>
+            
+        </div>
+    )
+}
+
+export default Body1;
